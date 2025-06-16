bf6129311a5d2dee4d6011f8746e05dbc8918394
catsia <katya.lemesh@gmail.com>
2025-06-16 23:20:33 +0300

    added animation hide + reworked managing signals

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -2,49 +2,20 @@ extends Node2D
 
 var next_scene = "res://scenes/scene1.tscn"
 var fader
+var dialog_manager
+var dialogSignalManager = preload("res://scripts/dialogic_signal_manager.gd")
 
 func _ready():
+	dialog_manager = dialogSignalManager.new()
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	Dialogic.timeline_ended.connect(_on_timeline_end)
 	Dialogic.start("intro")
 
-
 func _on_timeline_end():
 	fade_and_switch_scene()
 
-
-func _play_animation(signal_name: String):
-	var animation = get_node(signal_name)
-	if animation != null:
-		animation.play()
-	else:
-		print("No animation ", signal_name)
-
-
-func _show_image(image_path: String):
-	var texture = get_node(image_path)
-	if texture != null:
-		texture.visible = true
-	else:
-		print("No image ", image_path)	
-
-
-func _hide_image(image_path: String):
-	var texture = get_node(image_path)
-	if texture != null:
-		texture.visible = false
-	else:
-		print("No image ", image_path)	
-
-
 func _on_dialogic_signal(data: Dictionary, args = []):
-	if typeof(data) == TYPE_DICTIONARY:
-		if data.has("animation"):
-			_play_animation(data["animation"])
-		if data.has("show_image"):
-			_show_image(data["show_image"])
-		if data.has("hide_image"):
-			_hide_image(data["hide_image"])			
+	dialog_manager._manage_dialogic_signal(self, data)
 
 func fade_and_switch_scene():
 	var fade = get_node("/root/ScreenFade")
@@ -52,6 +23,5 @@ func fade_and_switch_scene():
 		get_tree().change_scene_to_file(next_scene)
 	)
 
-
 func _on_cosmos_animation_finished() -> void:
-	_play_animation("cosmos_cycle")
+	dialog_manager._play_animation(self, "cosmos_cycle")
---
+++ scripts/dialogic_signal_manager.gd
@@ -0,0 +1,41 @@
+extends Node
+
+func _manage_dialogic_signal(context: Node, data: Dictionary):
+	if typeof(data) == TYPE_DICTIONARY:
+		if data.has("animation"):
+			_play_animation(context, data["animation"])
+		if data.has("show_image"):
+			_show_image(context, data["show_image"])
+		if data.has("hide_image"):
+			_hide_image(context, data["hide_image"])
+		if data.has("hide_animation"):
+			_hide_animation(context, data["hide_image"])					
+
+func _play_animation(context: Node, signal_name: String):
+	var animation =  context.get_node_or_null(signal_name)
+	if animation:
+		animation.play()
+	else:
+		print("No animation ", signal_name)
+
+func _show_image(context: Node, image_path: String):
+	var texture = context.get_node_or_null(image_path)
+	if texture:
+		texture.visible = true
+	else:
+		print("No image ", image_path)	
+
+
+func _hide_image(context: Node, image_path: String):
+	var texture = context.get_node_or_null(image_path)
+	if texture:
+		texture.visible = false
+	else:
+		print("No image ", image_path)	
+
+func _hide_animation(context: Node, signal_name: String):
+	var animation = context.get_node_or_null(signal_name)
+	if animation:
+		animation.visible = false
+	else:
+		print("No animation ", signal_name)	
