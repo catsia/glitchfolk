9b2e2ecdb8269795df567424d2c8ade323c0e5ed
HederaGit <cruelteapot@gmail.com>
2025-09-20 12:55:57 +0300

    chapter 1 structure

---
+++ scenes/witch_1.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+var dialog_manager
+var timeline_manager
+
+var dialogSignalManager = preload("res://scripts/dialogic_signal_manager.gd")
+var timelineManager = preload("res://scripts/timeline_manager.gd")
+
+func _ready():
+	dialog_manager = dialogSignalManager.new()
+	timeline_manager = timelineManager.new()
+	
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.start("witch1")
+
+func _on_dialogic_signal(data: Dictionary, args = []):
+	dialog_manager._manage_dialogic_signal(self, data)	
