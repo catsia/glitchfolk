8e7178e758cbc25edd557848f18b37ab7ef9fab4
catsia <katya.lemesh@gmail.com>
2025-11-09 21:35:10 +0300

    reworked interactive images + anim on eye obraz + grimoire scene

--- scripts/global_variables.gd
+++ scripts/global_variables.gd
@@ -1,5 +1,9 @@
 extends Node
 
 var eye_key = "eye"
-var eye_state= "closed"
 var next = "next"
+var eye_looks_state = "eyeLooks"
+var eye_closed_state = "eyeCloses"
+
+var stop_all = "stop_all_interactive"
+var play_all = "play_all_interactive"
