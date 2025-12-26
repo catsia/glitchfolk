efd373c9b1d1078e7052852f44e04fc8fe5aaa22
HederaGit <cruelteapot@gmail.com>
2025-12-27 00:09:00 +0300

    Secret Guest
    new grass and eyes

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -7,5 +7,7 @@ func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
 func _ready(): 
+	#$CanvasLayer/MenuGrass.modulate.a = 0.0
+	#create_tween().tween_property($CanvasLayer/MenuGrass, "modulate:a", 1.0, 3.0)
 	await get_tree().create_timer(2.5).timeout
 	create_tween().tween_property($CanvasLayer/fade_out, "modulate:a", 0.0, 2.0)
