e4c712f802b8df9f33fe7cac028a6e6b8f75c6ce
HederaGit <cruelteapot@gmail.com>
2025-12-26 23:18:04 +0300

    Main Menu ver.2 (ultrafolk)

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,17 +1,11 @@
 extends Control
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/preintro.tscn")
-
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_start_but_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/preintro.tscn")
 
-
-
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _ready(): 
+	await get_tree().create_timer(2.5).timeout
+	create_tween().tween_property($CanvasLayer/fade_out, "modulate:a", 0.0, 2.0)
