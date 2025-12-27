7987a7645ba047a137f477a510613387ddc365cf
HederaGit <cruelteapot@gmail.com>
2025-12-27 06:51:17 +0300

    Pause menu

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
-func _on_resume_button_pressed() -> void:
+func _on_cont_button_pressed() -> void:
 	SignalManager.emit_resume_game()
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
